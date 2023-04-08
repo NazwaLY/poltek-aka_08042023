8bee83a229240a8320b2263079f876e76bf72af8
DESKTOP-4IAFLL7\Acer <nazwaly22@gmail.com>
2023-04-08 12:24:34 +0700

    commit

--- app.py
+++ app.py
@@ -17,3 +17,13 @@ if st.button('hitung'):
     st.write(f'hasil kali antara {number1} dan {number2} adalah {number1*number2}')
 else:
     st.write('silahkan klik tombol hitung')
+    
+import numpy as np
+array1 = np.random.randint(10,40, size=(10,))
+array2 = np.random.randint(10,40, size=(10,))
+
+import pandas as pd
+st.dataframe(pd.DataFrame({'kelas A': array1,
+                           'kelas B': array2
+                          })
+            )
